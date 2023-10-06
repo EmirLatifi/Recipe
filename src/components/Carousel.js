d6552d39b903c452927329e41b960f541e2c4d58
Emir Latifi <emirlatifi53@gmail.com>
2023-10-06 17:17:39 +0200

    last commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import AliceCarousel from 'react-alice-carousel';
 import { Link } from 'react-router-dom';
 import 'react-alice-carousel/lib/alice-carousel.css'
-import "../Style/styles.css"
+import "../style/styles.css"
 
 function Carousel() {
 
